d289af42e18ad6501c85243046f9e6fab725a289
Diana Gaston <Bahashem@yahoo.com>
2025-04-14 17:27:24 -0400

    added code and README

---
+++ Develop/server/cypres.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'cypress';
+import viteConfig from './vite.config';
+
+export default defineConfig({
+  component: {
+    port: 5173,
+    devServer: {
+      framework: 'react',
+      bundler: 'vite',
+      viteConfig,
+    },
+  },
+
+  e2e: {
+    baseUrl: 'http://localhost:3001',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});import { defineConfig } from "cypress";
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+    specPattern: "cypress/component/**/*.cy.{ts,tsx}",
+  },
+});
